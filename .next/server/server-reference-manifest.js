3d9357beac6a4eb8471c8c6fd19287742e38b072
bhagyalakshan <137475071+bhagyalakshan@users.noreply.github.com>
2026-01-29 16:38:05 +0530

    Refactor code structure for improved readability and maintainability

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"kucZSUJgSMzRFDddniQoFwgKzAnihWMliiPQbQtaMTw=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wSCnF5ZPC4LXVoPPnJAt7voyAhqBKHG7BW5w3a8E1KI=\"\n}"
